2810e98b7de385d146981e4727a981ab3928c445
Komal Sharma <sharmaakomal06@gmail.com>
2025-08-25 17:39:19 +0530

    Exception

---
+++ src/exception/YoungerAgeException.java
@@ -0,0 +1,29 @@
+package exception;// throws keyword usage
+// customize RunTimeException
+
+import java.util.Scanner;
+
+public class YoungerAgeException extends RuntimeException{
+    YoungerAgeException(String msg){
+        super(msg);
+    }
+
+}
+class Voting{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = sc.nextInt();
+        try {
+            if (age < 18) {
+                throw new YoungerAgeException("You are not eligible to vote.");
+            } else {
+                System.out.println("Proceed to vote!");
+            }
+        }
+        catch (YoungerAgeException e){
+            e.printStackTrace();
+        }
+        System.out.println("All steps executed");
+    }
+}
